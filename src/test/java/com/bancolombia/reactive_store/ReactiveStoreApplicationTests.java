fd5ab384b92031871b55277000d8caf697ec8913
edgardo.nvf <edgardo.velasco@netec.com>
2026-07-17 08:35:41 -0500

    test2

--- src/test/java/com/bancolombia/reactive_store/ReactiveStoreApplicationTests.java
+++ src/test/java/com/bancolombia/reactive_store/ReactiveStoreApplicationTests.java
@@ -9,10 +9,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.bancolombia.reactive_store.exception.ProductNotFoundException;
 import com.bancolombia.reactive_store.model.ProductEntity;
 import com.bancolombia.reactive_store.repository.IProductRepositorySQL;
+import com.bancolombia.reactive_store.service.ProductService;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 @ExtendWith(MockitoExtension.class)
@@ -20,6 +23,9 @@ class ReactiveStoreApplicationTests {
 
 	@Mock
 	IProductRepositorySQL repository;
+
+	@Mock
+	ProductService service;
 
 	@Test
 	void findAllTest(){
@@ -36,6 +42,18 @@ class ReactiveStoreApplicationTests {
 					.expectNext(product)
 					.verifyComplete();
 
+	}
+
+
+	@Test
+	void findByIdTest(){
+		/*flujo*/
+		when(service.findById(-1L))
+		.thenReturn(Mono.error(new ProductNotFoundException("no existe")));
+
+		StepVerifier.create(service.findById(-1L))
+					.expectError(ProductNotFoundException.class)
+					.verify();
 	}
 
 }
